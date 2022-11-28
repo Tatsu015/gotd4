4809888d9da72738518cb8661edab9fad1a233b3
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-11-28 23:15:56 +0900

    add register methods

--- internal/app/register.go
+++ internal/app/register.go
@@ -1,7 +1,17 @@
 package app
 
-type Register struct{}
+type Register struct {
+	v int16
+}
 
 func NewRegister() Register {
 	return Register{}
+}
+
+func (r *Register) value() int16 {
+	return r.v
+}
+
+func (r *Register) setValue(v int16) {
+	r.v = v
 }
