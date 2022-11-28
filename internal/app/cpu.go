91c1a7ab67f7e5ed18e40d82f248491279f9e065
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-11-28 23:28:24 +0900

    remove opecode file because base CPU

--- internal/app/cpu.go
+++ internal/app/cpu.go
@@ -5,6 +5,23 @@ import (
 	"time"
 )
 
+type Opecode int16
+
+const (
+	ADD_A  Opecode = 0x00
+	MOV_AB Opecode = 0x01
+	IN_A   Opecode = 0x02
+	MOV_A  Opecode = 0x03
+	MOV_BA Opecode = 0x04
+	ADD_B  Opecode = 0x05
+	IN_B   Opecode = 0x06
+	MOV_B  Opecode = 0x07
+	OUT_B  Opecode = 0x09
+	OUT    Opecode = 0x0b
+	JMP    Opecode = 0x0e
+	JNC    Opecode = 0x0f
+)
+
 type CPU struct {
 	a       Register
 	b       Register
