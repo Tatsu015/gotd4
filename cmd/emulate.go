0777340482bcc3ed8f9e511f211a18e094af3db2
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-12-02 23:03:06 +0900

    modify command name and specified rom file

--- cmd/emulate.go
+++ cmd/emulate.go
@@ -4,16 +4,15 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"io/ioutil"
 
 	"github.com/Tatsu015/gotd4.git/internal/app"
 	"github.com/spf13/cobra"
 )
 
-// runCmd represents the run command
-var runCmd = &cobra.Command{
-	Use:   "run",
+// emulateCmd represents the emulate command
+var emulateCmd = &cobra.Command{
+	Use:   "emulate",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -22,29 +21,31 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		f, _ := cmd.Flags().GetString("file")
+		if f != "" {
+			bytes, err := ioutil.ReadFile(f)
+			if err != nil {
+				panic(err)
+			}
+			rom := app.NewROM(bytes)
+			in := app.NewIO()
+			out := app.NewIO()
+			cpu := app.NewCPU(rom, in, out)
+			cpu.Run()
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(runCmd)
-	bytes, err := ioutil.ReadFile("./example/sample.bin")
-	if err != nil {
-		panic(err)
-	}
-	rom := app.NewROM(bytes)
-	in := app.NewIO()
-	out := app.NewIO()
-	cpu := app.NewCPU(rom, in, out)
-	cpu.Run()
-
+	rootCmd.AddCommand(emulateCmd)
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// emulateCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// emulateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	emulateCmd.Flags().StringP("file", "f", "", "emurate TD4 CPU using specified 'file' program")
 }
