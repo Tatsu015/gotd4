4daa931aa2ea9b254dda2f8558a022d92f193f64
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-12-03 15:19:43 +0900

    modify displaying

--- internal/emulator/io/output.go
+++ internal/emulator/io/output.go
@@ -2,6 +2,9 @@ package io
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/Tatsu015/gotd4.git/internal/define"
 )
@@ -14,8 +17,20 @@ func NewOutput() Output {
 	return Output{}
 }
 
+func (o *Output) replace01(org string, zero string, one string) string {
+	t1 := strings.ReplaceAll(org, "0", zero)
+	t2 := strings.ReplaceAll(t1, "1", one)
+	return t2
+}
+
 func (o *Output) Show() {
-	fmt.Printf("%04b\n", o.v)
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+	bitStr := fmt.Sprintf("%04b\n", o.v)
+	disp := o.replace01(bitStr, "□", "■")
+	fmt.Print(disp)
 }
 
 func (o *Output) Value() int {
