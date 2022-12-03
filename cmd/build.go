a0dd2e682d63e6d0040796509f9e0ae7d4c90710
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-12-04 00:45:08 +0900

    add build --out option

--- cmd/build.go
+++ cmd/build.go
@@ -17,12 +17,16 @@ var buildCmd = &cobra.Command{
 	Short: "Build TD4 assembler file to binary",
 	Long:  "Build TD4 assembler file to binary. assembler file extension is 'td4asm'.",
 	Run: func(cmd *cobra.Command, args []string) {
-		f, _ := cmd.Flags().GetString("file")
-		c := compiler.NewCompiler(f)
+		srcPath, _ := cmd.Flags().GetString("file")
+		newPath, _ := cmd.Flags().GetString("out")
+		if newPath == "" {
+			newPath = srcPath[:strings.LastIndex(srcPath, ".")]
+		}
+
+		c := compiler.NewCompiler(srcPath)
 		b := c.Compile()
 
-		newpath := f[:strings.LastIndex(f, ".")]
-		err := ioutil.WriteFile(newpath, b, 0755)
+		err := ioutil.WriteFile(newPath, b, 0755)
 		if err != nil {
 			panic(err)
 		}
@@ -34,4 +38,5 @@ func init() {
 
 	buildCmd.Flags().StringP("file", "f", "", "Build TD4 assembler file.")
 	buildCmd.MarkFlagRequired("file")
+	buildCmd.Flags().StringP("out", "o", "", "Build TD4 binary file output path.")
 }
