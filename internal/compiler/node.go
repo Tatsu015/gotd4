9eeab00f29912849360faf51770aa48f6d759b90
Tatsu015 <tatsumi.soichiro@gmail.com>
2022-12-03 23:39:28 +0900

    complete node to binary data

--- internal/compiler/node.go
+++ internal/compiler/node.go
@@ -13,7 +13,16 @@ func (n *Node) add(c Node) {
 	n.children = append(n.children, c)
 }
 
+func (n *Node) ntob() []byte {
+	t := []byte{}
+	for _, c := range n.children {
+		b := c.ntob()
+		t = append(t, b...)
+	}
+	return append(t, byte(n.val))
+}
+
 func (n *Node) toBinary() []byte {
-	ml := []byte{}
+	ml := n.ntob()
 	return ml
 }
